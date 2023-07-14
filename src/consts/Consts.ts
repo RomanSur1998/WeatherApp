0e113069982fac5c2ffb4af63d1cce1286898deb
RomanSur1998 <123935997+RomanSur1998@users.noreply.github.com>
2023-07-14 16:25:30 +0600

    added api

--- src/consts/Consts.ts
+++ src/consts/Consts.ts
@@ -2,6 +2,6 @@ import { useSelector } from "react-redux";
 import { LocationState } from "../store/LoactionReducer";
 
 export const getAPIUrl = (city: string) =>
-  `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=7`;
+  `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=7`;
 
 export const API_KEY = "50cde8b11b3a4d6189570301230807";
